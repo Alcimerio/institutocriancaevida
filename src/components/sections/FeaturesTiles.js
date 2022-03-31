e762d2796178802929b038af7365ddc7c9e7c290
Alcimerio Rangel <alcimerioj@id.uff.br>
2022-03-31 19:20:57 -0300

    name

--- src/components/sections/FeaturesTiles.js
+++ src/components/sections/FeaturesTiles.js
@@ -133,7 +133,7 @@ const FeaturesTiles = ({
                 </div>
                 <div className="features-tiles-item-content">
                   <h4 className="mt-0 mb-8">
-                    Rayanne Silva
+                    Rayanne Freimann
                   </h4>
                 </div>
               </div>
